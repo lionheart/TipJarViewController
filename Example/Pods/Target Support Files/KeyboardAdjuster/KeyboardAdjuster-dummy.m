6955d582f04a0196c907c32af716ac59016acb6f
Dan Loewenherz <dan@lionheartsw.com>
2018-05-06 13:06:02 -0500

    update pod target support files

---
+++ Example/Pods/Target Support Files/KeyboardAdjuster/KeyboardAdjuster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KeyboardAdjuster : NSObject
+@end
+@implementation PodsDummy_KeyboardAdjuster
+@end
---
+++ Example/Pods/Target Support Files/QuickTableView/QuickTableView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QuickTableView : NSObject
+@end
+@implementation PodsDummy_QuickTableView
+@end
