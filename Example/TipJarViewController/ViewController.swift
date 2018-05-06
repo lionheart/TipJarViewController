8ad21ad6f7a546398fa8bfa98a5b4665c37faa3a
Dan Loewenherz <dan@lionheartsw.com>
2018-05-06 12:52:45 -0500

    Initial commit

---
+++ Example/TipJarViewController/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TipJarViewController
+//
+//  Created by Dan Loewenherz on 05/06/2018.
+//  Copyright (c) 2018 Dan Loewenherz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
