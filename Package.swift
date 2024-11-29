c2b1c2f0c4d6236623fae25b6f83b204beb5b15f
Dan Loewenherz <dan@lionheartsw.com>
2024-11-29 16:00:52 -0600

    migrate to SPM

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "TipJarViewController",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "TipJarViewController",
+            targets: ["TipJarViewController"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/lionheart/QuickTableView.git", from: "5.0.0"),
+        .package(url: "https://github.com/lionheart/SuperLayout.git", from: "4.0.0"),
+        .package(url: "https://github.com/lionheart/LionheartExtensions.git", from: "6.0.0")
+    ],
+    targets: [
+        .target(
+            name: "TipJarViewController",
+            dependencies: [
+                .product(name: "QuickTableView", package: "QuickTableView"),
+                .product(name: "SuperLayout", package: "SuperLayout"),
+                .product(name: "LionheartExtensions", package: "LionheartExtensions")
+            ],
+            path: "TipJarViewController/Classes",
+            resources: [
+                .process("Assets")
+            ]
+        ),
+        .testTarget(
+            name: "TipJarViewControllerTests",
+            dependencies: ["TipJarViewController"]
+        )
+    ],
+    swiftLanguageModes: [.v4]
+)
